4d6a3ce05d339e91975851967e51c94edf358cfb
papai <Papai Pc@Lenovo.com>
2023-04-01 00:37:08 +0530

    success

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,25 @@ import React from "react";
 import "../styles/App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./Login";
+import Home from "./Home";
+
+import Header from "./Header";
+
 const App = () => {
   return (
     <div id="main">
       <Routes>
-        <Route path="/" element={<Login />} />
+        {/* <Route exact path="/" /><Home /></Routes> */}
+        {/* <Route path="/" element={<Login />} /> */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Home />
+            </>
+          }
+        />
       </Routes>
     </div>
   );
